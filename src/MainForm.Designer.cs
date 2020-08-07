85eb7ec261c7b08ce8b7dd1285ce1ed7e5a7f9b5
Dj_Mike238 <mike238dimaggio@gmail.com>
2020-08-07 17:24:57 +0200

    MainForm: UI tweaks

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -31,12 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.addLineBtn = new System.Windows.Forms.Button();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.newProjBtn = new System.Windows.Forms.ToolStripButton();
-            this.openProjBtn = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.saveProjBtn = new System.Windows.Forms.ToolStripButton();
-            this.saveAsProjBtn = new System.Windows.Forms.ToolStripButton();
-            this.projSettingsBtn = new System.Windows.Forms.ToolStripButton();
             this.fileListBox = new System.Windows.Forms.CheckedListBox();
             this.listView = new System.Windows.Forms.ListView();
             this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -46,6 +41,12 @@
             this.proposal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.comment = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.editLineBtn = new System.Windows.Forms.Button();
+            this.delLineBtn = new System.Windows.Forms.Button();
+            this.newProjBtn = new System.Windows.Forms.ToolStripButton();
+            this.openProjBtn = new System.Windows.Forms.ToolStripButton();
+            this.saveProjBtn = new System.Windows.Forms.ToolStripButton();
+            this.saveAsProjBtn = new System.Windows.Forms.ToolStripButton();
+            this.projSettingsBtn = new System.Windows.Forms.ToolStripButton();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -60,6 +61,7 @@
             this.addLineBtn.TabIndex = 2;
             this.addLineBtn.Text = "Add Line";
             this.addLineBtn.UseVisualStyleBackColor = true;
+            this.addLineBtn.Click += new System.EventHandler(this.addLineBtn_Click);
             // 
             // toolStrip1
             // 
@@ -76,57 +78,10 @@
             this.toolStrip1.TabIndex = 3;
             this.toolStrip1.Text = "toolStrip1";
             // 
-            // newProjBtn
-            // 
-            this.newProjBtn.Image = ((System.Drawing.Image)(resources.GetObject("newProjBtn.Image")));
-            this.newProjBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.newProjBtn.Name = "newProjBtn";
-            this.newProjBtn.Size = new System.Drawing.Size(91, 22);
-            this.newProjBtn.Text = "New Project";
-            this.newProjBtn.Click += new System.EventHandler(this.newProjBtn_Click);
-            // 
-            // openProjBtn
-            // 
-            this.openProjBtn.Image = ((System.Drawing.Image)(resources.GetObject("openProjBtn.Image")));
-            this.openProjBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.openProjBtn.Name = "openProjBtn";
-            this.openProjBtn.Size = new System.Drawing.Size(96, 22);
-            this.openProjBtn.Text = "Open Project";
-            this.openProjBtn.Click += new System.EventHandler(this.openProjBtn_Click);
-            // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
             this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
-            // 
-            // saveProjBtn
-            // 
-            this.saveProjBtn.Enabled = false;
-            this.saveProjBtn.Image = ((System.Drawing.Image)(resources.GetObject("saveProjBtn.Image")));
-            this.saveProjBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.saveProjBtn.Name = "saveProjBtn";
-            this.saveProjBtn.Size = new System.Drawing.Size(91, 22);
-            this.saveProjBtn.Text = "Save Project";
-            // 
-            // saveAsProjBtn
-            // 
-            this.saveAsProjBtn.Enabled = false;
-            this.saveAsProjBtn.Image = ((System.Drawing.Image)(resources.GetObject("saveAsProjBtn.Image")));
-            this.saveAsProjBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.saveAsProjBtn.Name = "saveAsProjBtn";
-            this.saveAsProjBtn.Size = new System.Drawing.Size(116, 22);
-            this.saveAsProjBtn.Text = "Save Project As...";
-            // 
-            // projSettingsBtn
-            // 
-            this.projSettingsBtn.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-            this.projSettingsBtn.Enabled = false;
-            this.projSettingsBtn.Image = ((System.Drawing.Image)(resources.GetObject("projSettingsBtn.Image")));
-            this.projSettingsBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.projSettingsBtn.Name = "projSettingsBtn";
-            this.projSettingsBtn.Size = new System.Drawing.Size(109, 22);
-            this.projSettingsBtn.Text = "Project Settings";
-            this.projSettingsBtn.Click += new System.EventHandler(this.projSettingsBtn_Click);
             // 
             // fileListBox
             // 
@@ -142,6 +97,7 @@
             // 
             // listView
             // 
+            this.listView.Activation = System.Windows.Forms.ItemActivation.TwoClick;
             this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
@@ -161,10 +117,12 @@
             this.listView.MultiSelect = false;
             this.listView.Name = "listView";
             this.listView.Size = new System.Drawing.Size(854, 414);
+            this.listView.Sorting = System.Windows.Forms.SortOrder.Ascending;
             this.listView.TabIndex = 5;
             this.listView.UseCompatibleStateImageBehavior = false;
             this.listView.View = System.Windows.Forms.View.Details;
             this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            this.listView.DoubleClick += new System.EventHandler(this.listView_DoubleClick);
             // 
             // id
             // 
@@ -206,12 +164,73 @@
             this.editLineBtn.TabIndex = 6;
             this.editLineBtn.Text = "Edit Line";
             this.editLineBtn.UseVisualStyleBackColor = true;
+            this.editLineBtn.Click += new System.EventHandler(this.editLineBtn_Click);
+            // 
+            // delLineBtn
+            // 
+            this.delLineBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.delLineBtn.Enabled = false;
+            this.delLineBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delLineBtn.Location = new System.Drawing.Point(605, 448);
+            this.delLineBtn.Name = "delLineBtn";
+            this.delLineBtn.Size = new System.Drawing.Size(159, 42);
+            this.delLineBtn.TabIndex = 7;
+            this.delLineBtn.Text = "Delete Line";
+            this.delLineBtn.UseVisualStyleBackColor = true;
+            // 
+            // newProjBtn
+            // 
+            this.newProjBtn.Image = ((System.Drawing.Image)(resources.GetObject("newProjBtn.Image")));
+            this.newProjBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newProjBtn.Name = "newProjBtn";
+            this.newProjBtn.Size = new System.Drawing.Size(91, 22);
+            this.newProjBtn.Text = "New Project";
+            this.newProjBtn.Click += new System.EventHandler(this.newProjBtn_Click);
+            // 
+            // openProjBtn
+            // 
+            this.openProjBtn.Image = ((System.Drawing.Image)(resources.GetObject("openProjBtn.Image")));
+            this.openProjBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openProjBtn.Name = "openProjBtn";
+            this.openProjBtn.Size = new System.Drawing.Size(96, 22);
+            this.openProjBtn.Text = "Open Project";
+            this.openProjBtn.Click += new System.EventHandler(this.openProjBtn_Click);
+            // 
+            // saveProjBtn
+            // 
+            this.saveProjBtn.Enabled = false;
+            this.saveProjBtn.Image = ((System.Drawing.Image)(resources.GetObject("saveProjBtn.Image")));
+            this.saveProjBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveProjBtn.Name = "saveProjBtn";
+            this.saveProjBtn.Size = new System.Drawing.Size(91, 22);
+            this.saveProjBtn.Text = "Save Project";
+            // 
+            // saveAsProjBtn
+            // 
+            this.saveAsProjBtn.Enabled = false;
+            this.saveAsProjBtn.Image = ((System.Drawing.Image)(resources.GetObject("saveAsProjBtn.Image")));
+            this.saveAsProjBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveAsProjBtn.Name = "saveAsProjBtn";
+            this.saveAsProjBtn.Size = new System.Drawing.Size(116, 22);
+            this.saveAsProjBtn.Text = "Save Project As...";
+            // 
+            // projSettingsBtn
+            // 
+            this.projSettingsBtn.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.projSettingsBtn.Enabled = false;
+            this.projSettingsBtn.Image = ((System.Drawing.Image)(resources.GetObject("projSettingsBtn.Image")));
+            this.projSettingsBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.projSettingsBtn.Name = "projSettingsBtn";
+            this.projSettingsBtn.Size = new System.Drawing.Size(109, 22);
+            this.projSettingsBtn.Text = "Project Settings";
+            this.projSettingsBtn.Click += new System.EventHandler(this.projSettingsBtn_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1106, 502);
+            this.Controls.Add(this.delLineBtn);
             this.Controls.Add(this.fileListBox);
             this.Controls.Add(this.listView);
             this.Controls.Add(this.toolStrip1);
@@ -246,6 +265,7 @@
         private System.Windows.Forms.ColumnHeader proposal;
         private System.Windows.Forms.ColumnHeader comment;
         private System.Windows.Forms.Button editLineBtn;
+        private System.Windows.Forms.Button delLineBtn;
     }
 }
 
