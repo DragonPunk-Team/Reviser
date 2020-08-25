5ff8bc455d55edd217fc897733e303c1f1d17efa
Dj_Mike238 <mike238dimaggio@gmail.com>
2020-08-26 01:37:42 +0200

    LineEditor: UI tweaks

--- src/LineEditor.Designer.cs
+++ src/LineEditor.Designer.cs
@@ -39,10 +39,18 @@
             this.origLblText = new System.Windows.Forms.Label();
             this.tranTextBox = new System.Windows.Forms.Label();
             this.origTextBox = new System.Windows.Forms.Label();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.commentBox = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             ((System.ComponentModel.ISupportInitialize)(this.tranLblPicBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.origPicBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tranPicBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.origLblPicBox)).BeginInit();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -157,11 +165,65 @@
             this.origTextBox.TabIndex = 10;
             this.origTextBox.Text = "origTextBox";
             // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.commentBox);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(402, 181);
+            this.toolStripContainer1.Location = new System.Drawing.Point(12, 216);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(402, 206);
+            this.toolStripContainer1.TabIndex = 12;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // commentBox
+            // 
+            this.commentBox.Location = new System.Drawing.Point(0, 0);
+            this.commentBox.Multiline = true;
+            this.commentBox.Name = "commentBox";
+            this.commentBox.Size = new System.Drawing.Size(402, 181);
+            this.commentBox.TabIndex = 0;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(339, 428);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 13;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(258, 428);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 14;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Location = new System.Drawing.Point(3, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(111, 25);
+            this.toolStrip1.TabIndex = 0;
+            // 
             // LineEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(430, 458);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.toolStripContainer1);
             this.Controls.Add(this.origTextBox);
             this.Controls.Add(this.tranTextBox);
             this.Controls.Add(this.origLblText);
@@ -173,6 +235,9 @@
             this.Controls.Add(this.searchBtn);
             this.Controls.Add(this.idBox);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "LineEditor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LineEditor";
@@ -181,6 +246,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.origPicBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tranPicBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.origLblPicBox)).EndInit();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.ContentPanel.PerformLayout();
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -199,5 +270,10 @@
         private System.Windows.Forms.Label origLblText;
         private System.Windows.Forms.Label tranTextBox;
         private System.Windows.Forms.Label origTextBox;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.TextBox commentBox;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button cancelBtn;
     }
 }
