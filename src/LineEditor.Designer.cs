4b902388548411948ebe462f78cf56a994c313db
Dj_Mike238 <mike238dimaggio@gmail.com>
2020-12-02 19:57:04 +0100

    LineEditor: UX tweaks
    Now you can press Enter to trigger the Search button after inserting a Line ID.

--- src/LineEditor.Designer.cs
+++ src/LineEditor.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Reviser
+namespace Reviser
 {
     partial class LineEditor
     {
@@ -89,6 +89,7 @@
             // 
             // commentBox
             // 
+            this.commentBox.AcceptsReturn = true;
             this.commentBox.Location = new System.Drawing.Point(0, 0);
             this.commentBox.Multiline = true;
             this.commentBox.Name = "commentBox";
@@ -161,6 +162,7 @@
             // 
             // LineEditor
             // 
+            this.AcceptButton = this.searchBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(430, 458);
