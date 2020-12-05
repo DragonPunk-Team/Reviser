6721f2523fa9cea738503f0de36774144c97369b
Dj_Mike238 <mike238dimaggio@gmail.com>
2020-12-05 18:37:13 +0100

    LineEditor: UI tweaks

--- src/LineEditor.Designer.cs
+++ src/LineEditor.Designer.cs
@@ -118,7 +118,7 @@
             this.toolStrip1.Location = new System.Drawing.Point(3, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolStrip1.Size = new System.Drawing.Size(126, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(95, 25);
             this.toolStrip1.TabIndex = 0;
             // 
             // copyLineBtn
