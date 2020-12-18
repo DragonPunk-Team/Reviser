b04ec97cd75cf3081dcf32588780f0ee043b76fa
Dj_Mike238 <mike238dimaggio@gmail.com>
2020-12-19 00:09:03 +0100

    LineEditor: UI tweaks

--- src/LineEditor.Designer.cs
+++ src/LineEditor.Designer.cs
@@ -99,6 +99,7 @@ namespace Reviser
             // toolStrip1
             // 
             this.toolStrip1.AutoSize = false;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
             this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.copyLineBtn,
