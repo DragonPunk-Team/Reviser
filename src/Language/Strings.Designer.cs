2a93b0095aea79aa58c028ed11b8d30118979190
Dj_Mike238 <mike238dimaggio@gmail.com>
2021-08-14 00:20:54 +0200

    Add localization files for all the game characters
    ...and move some files around

--- src/Language/Strings.Designer.cs
+++ src/Language/Strings.Designer.cs
@@ -39,7 +39,7 @@ namespace Reviser.Language {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Reviser.src.lang.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Reviser.src.Language.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
