7fb9586972584a2e108b0dbe607c22f114a44fde
Dj_Mike238 <mike238dimaggio@gmail.com>
2021-10-05 00:24:28 +0200

    ProjectFile: revert cleanups
    b5944893d531081bf05fc20256ed273441da7fc2 broke creating and editing projects. This commit fixes those.

--- src/ProjectFile.cs
+++ src/ProjectFile.cs
@@ -41,16 +41,16 @@ namespace Reviser
 
         public void ReadProject()
         {
-            byte[] compressed = Convert.FromBase64String(File.ReadAllText(path));
-            byte[] decompressed = Decompress(compressed);
+            var compressed = Convert.FromBase64String(File.ReadAllText(path));
+            var decompressed = Decompress(compressed);
 
             project = JsonConvert.DeserializeObject<Project>(Encoding.UTF8.GetString(decompressed));
         }
 
         public void WriteProject()
         {
-            byte[] encoded = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(project));
-            byte[] compressed = Compress(encoded);
+            var encoded = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(project));
+            var compressed = Compress(encoded);
 
             File.WriteAllText(path, Convert.ToBase64String(compressed));
         }
@@ -65,29 +65,36 @@ namespace Reviser
 
         private static byte[] Compress(byte[] input)
         {
-            var result = new MemoryStream();
-            var lengthBytes = BitConverter.GetBytes(input.Length);
-            result.Write(lengthBytes, 0, 4);
+            using (var result = new MemoryStream())
+            {
+                var lengthBytes = BitConverter.GetBytes(input.Length);
+                result.Write(lengthBytes, 0, 4);
 
-            var compressionStream = new GZipStream(result, CompressionMode.Compress);
-            compressionStream.Write(input, 0, input.Length);
-            compressionStream.Flush();
+                using (var compressionStream = new GZipStream(result, CompressionMode.Compress))
+                {
+                    compressionStream.Write(input, 0, input.Length);
+                    compressionStream.Flush();
+                }
 
-            return result.ToArray();
+                return result.ToArray();
+            }
         }
 
         private static byte[] Decompress(byte[] input)
         {
-            var source = new MemoryStream(input);
-            var lengthBytes = new byte[4];
-            source.Read(lengthBytes, 0, 4);
+            using (var source = new MemoryStream(input))
+            {
+                var lengthBytes = new byte[4];
+                source.Read(lengthBytes, 0, 4);
 
-            var length = BitConverter.ToInt32(lengthBytes, 0);
-            var decompressionStream = new GZipStream(source, CompressionMode.Decompress);
-            var result = new byte[length];
-
-            decompressionStream.Read(result, 0, length);
-            return result;
+                var length = BitConverter.ToInt32(lengthBytes, 0);
+                using (var decompressionStream = new GZipStream(source, CompressionMode.Decompress))
+                {
+                    var result = new byte[length];
+                    decompressionStream.Read(result, 0, length);
+                    return result;
+                }
+            }
         }
     }
 }

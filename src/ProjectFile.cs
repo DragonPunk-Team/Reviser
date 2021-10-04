DragonPunk-Team/Reviser

commit b5944893d531081bf05fc20256ed273441da7fc2

General code cleanup

src/ProjectFile.cs
@@ -65,36 +65,29 @@ namespace Reviser
 
         private static byte[] Compress(byte[] input)
         {
-            using (var result = new MemoryStream())
-            {
-                var lengthBytes = BitConverter.GetBytes(input.Length);
-                result.Write(lengthBytes, 0, 4);
+            var result = new MemoryStream();
+            var lengthBytes = BitConverter.GetBytes(input.Length);
+            result.Write(lengthBytes, 0, 4);
 
-                using (var compressionStream = new GZipStream(result, CompressionMode.Compress))
-                {
-                    compressionStream.Write(input, 0, input.Length);
-                    compressionStream.Flush();
-                }
+            var compressionStream = new GZipStream(result, CompressionMode.Compress);
+            compressionStream.Write(input, 0, input.Length);
+            compressionStream.Flush();
 
-                return result.ToArray();
-            }
+            return result.ToArray();
         }
 
         private static byte[] Decompress(byte[] input)
         {
-            using (var source = new MemoryStream(input))
-            {
-                byte[] lengthBytes = new byte[4];
-                source.Read(lengthBytes, 0, 4);
+            var source = new MemoryStream(input);
+            var lengthBytes = new byte[4];
+            source.Read(lengthBytes, 0, 4);
 
-                var length = BitConverter.ToInt32(lengthBytes, 0);
-                using (var decompressionStream = new GZipStream(source, CompressionMode.Decompress))
-                {
-                    var result = new byte[length];
-                    decompressionStream.Read(result, 0, length);
-                    return result;
-                }
-            }
+            var length = BitConverter.ToInt32(lengthBytes, 0);
+            var decompressionStream = new GZipStream(source, CompressionMode.Decompress);
+            var result = new byte[length];
+
+            decompressionStream.Read(result, 0, length);
+            return result;
         }
     }
 }

Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Reviser

Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Le informazioni generali relative a un assembly sono controllate dal seguente 

src/MainForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Reviser
+﻿using Reviser.Tweaks;
+
+namespace Reviser
 {
     partial class MainForm
     {

src/files/Utils.cs
@@ -6,7 +6,7 @@ namespace Reviser.Files
     public static class Utils
     {
         #region Regex
-        private static Regex idRx = new (@"([0-9]*-?)*", RegexOptions.Compiled);
+        private static readonly Regex idRx = new (@"([0-9]*-?)*", RegexOptions.Compiled);
         #endregion
 
         public static int[] GetIDList(string lineIDs)
@@ -22,7 +22,7 @@ namespace Reviser.Files
                 if (id.Value.Contains("-"))
                 {
                     var split = id.Value.Split('-');
-                    var limits = new int[] { int.Parse(split[0]), int.Parse(split[1]) };
+                    var limits = new[] { int.Parse(split[0]), int.Parse(split[1]) };
 
                     for (var i = limits[0]; i <= limits[1]; i++)
                         idList.Add(i);

src/le/InsertLineID.cs
@@ -6,7 +6,7 @@ namespace Reviser.LE
 {
     public partial class InsertLineID : Form
     {
-        private Regex idRx = new Regex(@"[^0-9-, ]*", RegexOptions.Compiled);
+        private readonly Regex idRx = new(@"[^0-9-, ]*", RegexOptions.Compiled);
         public InsertLineID()
         {
             InitializeComponent();

src/le/LineEditor.cs
@@ -207,7 +207,7 @@ namespace Reviser.LE
             string currentLine;
             var gmd = GameFile.Get(projectType);
 
-            if (tranLines.Count() == 1)
+            if (tranLines.Length == 1)
             {
                 currentLine = tranLines[0].Item2;
 

src/le/PrevLinesEditor.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 using Reviser.Files;
@@ -11,11 +10,6 @@ namespace Reviser.LE
         IFile origFile;
         IFile tranFile;
         public string lineId;
-
-        #region Regex
-        private readonly Regex idRx = new(@"[^0-9-, ]*", RegexOptions.Compiled);
-        private readonly Regex sepRx = new(@"(-|,| )+", RegexOptions.Compiled);
-        #endregion
 
         public PrevLinesEditor(IFile origFile, IFile tranFile, string lineId)
         {

commit 7fb9586972584a2e108b0dbe607c22f114a44fde

ProjectFile: revert cleanups
b5944893d531081bf05fc20256ed273441da7fc2 broke creating and editing projects. This commit fixes those.

src/ProjectFile.cs
@@ -41,16 +41,16 @@ namespace Reviser
 
         public void ReadProject()
         {
-            byte[] compressed = Convert.FromBase64String(File.ReadAllText(path));
-            byte[] decompressed = Decompress(compressed);
+            var compressed = Convert.FromBase64String(File.ReadAllText(path));
+            var decompressed = Decompress(compressed);
 
             project = JsonConvert.DeserializeObject<Project>(Encoding.UTF8.GetString(decompressed));
         }
 
         public void WriteProject()
         {
-            byte[] encoded = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(project));
-            byte[] compressed = Compress(encoded);
+            var encoded = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(project));
+            var compressed = Compress(encoded);
 
             File.WriteAllText(path, Convert.ToBase64String(compressed));
         }
@@ -65,29 +65,36 @@ namespace Reviser
 
         private static byte[] Compress(byte[] input)
         {
-            var result = new MemoryStream();
-            var lengthBytes = BitConverter.GetBytes(input.Length);
-            result.Write(lengthBytes, 0, 4);
+            using (var result = new MemoryStream())
+            {
+                var lengthBytes = BitConverter.GetBytes(input.Length);
+                result.Write(lengthBytes, 0, 4);
 
-            var compressionStream = new GZipStream(result, CompressionMode.Compress);
-            compressionStream.Write(input, 0, input.Length);
-            compressionStream.Flush();
+                using (var compressionStream = new GZipStream(result, CompressionMode.Compress))
+                {
+                    compressionStream.Write(input, 0, input.Length);
+                    compressionStream.Flush();
+                }
 
-            return result.ToArray();
+                return result.ToArray();
+            }
         }
 
         private static byte[] Decompress(byte[] input)
         {
-            var source = new MemoryStream(input);
-            var lengthBytes = new byte[4];
-            source.Read(lengthBytes, 0, 4);
+            using (var source = new MemoryStream(input))
+            {
+                var lengthBytes = new byte[4];
+                source.Read(lengthBytes, 0, 4);
 
-            var length = BitConverter.ToInt32(lengthBytes, 0);
-            var decompressionStream = new GZipStream(source, CompressionMode.Decompress);
-            var result = new byte[length];
-
-            decompressionStream.Read(result, 0, length);
-            return result;
+                var length = BitConverter.ToInt32(lengthBytes, 0);
+                using (var decompressionStream = new GZipStream(source, CompressionMode.Decompress))
+                {
+                    var result = new byte[length];
+                    decompressionStream.Read(result, 0, length);
+                    return result;
+                }
+            }
         }
     }
 }