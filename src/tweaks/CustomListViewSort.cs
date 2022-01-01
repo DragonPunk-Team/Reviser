4b202647b2d48fd36d6a85f3e5c8b5174cd845b1
Dj_Mike238 <mike238dimaggio@gmail.com>
2022-01-01 13:14:57 +0100

    CustomListViewSort: restore intended sorting
    This reverts part of 61151722106c6619dd5d056f3862a7a434a09ca7.

--- src/tweaks/CustomListViewSort.cs
+++ src/tweaks/CustomListViewSort.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Reviser.Tweaks
 {
     internal class CustomListViewSort : IComparer
     {
+        private readonly Regex rx = new(@"[0-9]*", RegexOptions.Compiled);
+
         public int Compare(object x, object y)
         {
-            var intX = (int)(x as ListViewItem).Tag;
-            var intY = (int)(y as ListViewItem).Tag;
+            var intX = int.Parse(rx.Match((x as ListViewItem).SubItems[0].Text).Value);
+            var intY = int.Parse(rx.Match((y as ListViewItem).SubItems[0].Text).Value);
 
             if (intX < intY)
                 return -1;
